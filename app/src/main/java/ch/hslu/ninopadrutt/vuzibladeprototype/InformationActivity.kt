01089417877b1082f9e1d655e9a17cba39ea0194
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-04-02 08:57:04 +0200

    implement mainpage.

--- app/src/main/java/ch/hslu/ninopadrutt/vuzibladeprototype/InformationActivity.kt
+++ app/src/main/java/ch/hslu/ninopadrutt/vuzibladeprototype/InformationActivity.kt
@@ -1,25 +1,22 @@
 package ch.hslu.ninopadrutt.vuzibladeprototype
 
 import android.content.Intent
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
 import com.vuzix.hud.actionmenu.ActionMenuActivity
 
-class WarningActivity : ActionMenuActivity() {
+class InformationActivity : ActionMenuActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_warning)
+        setContentView(R.layout.activity_information)
     }
 
     override fun dispatchKeyEvent(event: KeyEvent): Boolean {
 
-        if (event.keyCode == KeyEvent.KEYCODE_DPAD_RIGHT) {
-            val intent = Intent(context, InformationActivity::class.java)
-            startActivity(intent)
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-        } else if (event.keyCode == KeyEvent.KEYCODE_DPAD_LEFT) {
-            val intent = Intent(context, MainActivity::class.java)
+        if (event.keyCode == KeyEvent.KEYCODE_DPAD_LEFT) {
+            val intent = Intent(context, WarningActivity::class.java)
             startActivity(intent)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         }
